c07facddef20919f8158e7344eb7e12622c7d8ba
Prashant Srivastav <srivastavp891@gmail.com>
2023-08-29 01:07:23 +0530

    Server setup and socket implement

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const path = require("path");
-
+const port = process.env.PORT || 5000;
 const app = express();
 const server = require("http").createServer(app);
 
+const io = require("socket.io")(server);
+
 app.use(express.static(path.join(__dirname, "public")));
 
-const port = process.env.PORT || 5000;
+io.on("connection", function(socket){
+  socket.on("newUser", function(username){
+    socket.broadcast.emit("update", username + " joined the conversation."); // Corrected spelling of "broadcast"
+  });
+  socket.on("exitUser", function(username){
+    socket.broadcast.emit("update", username + " left the conversation."); // Corrected spelling of "broadcast"
+  });
+  socket.on("chat", function(message){
+    socket.broadcast.emit("chat", message); // Corrected spelling of "broadcast"
+  });
+})
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
